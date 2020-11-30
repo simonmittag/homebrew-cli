9de9819e2e953b2c109bab1bc5a70a775d4c33fb
simon.mittag <simonmittag@gmail.com>
2020-12-01 07:31:50 +1100

    sha

--- Formula/mse6@0.2.10.rb
+++ Formula/mse6@0.2.10.rb
@@ -2,7 +2,7 @@ class Mse6AT0210 < Formula
   desc "HTTP/1.1 server implementing mock responses for testing of edge cases"
   homepage "https://github.com/simonmittag/mse6"
   url "https://github.com/simonmittag/mse6/archive/v0.2.10.tar.gz"
-  sha256 "b604b77e0a29f470009afd43bc997a6b8263e646eff78ca36b3105cd165a4778"
+  sha256 "698e117439289145aab0cf8e046fbc93ae8c8259891a9607cd95d6fb7ecfb838"
 
   depends_on "go" => :build
 
