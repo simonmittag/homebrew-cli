9943ba980e6d43a4712518141a895cc855cd0660
simon.mittag <simonmittag@gmail.com>
2020-12-12 09:04:03 +1100

    updated bad hash

--- Formula/mse6@0.2.12.rb
+++ Formula/mse6@0.2.12.rb
@@ -2,7 +2,7 @@ class Mse6AT0212 < Formula
   desc "HTTP/1.1 server implementing mock responses for testing of edge cases"
   homepage "https://github.com/simonmittag/mse6"
   url "https://github.com/simonmittag/mse6/archive/v0.2.12.tar.gz"
-  sha256 "865ea89df0b9c25842489c5958dea3e02db8e423acbee168a40b490f6ca88677"
+  sha256 "1c81be1799c36bc83866d1581f541c71fa5e318c753a472abd8ba5aa8395c20b"
 
   depends_on "go" => :build
 
