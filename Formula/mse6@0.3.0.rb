5ac7cd880c4d34a4e0601ab8ae8a280da64812b8
simon.mittag <simonmittag@gmail.com>
2021-03-27 08:33:31 +1100

    v0.3.0

---
+++ Formula/mse6@0.3.0.rb
@@ -0,0 +1,23 @@
+class Mse6AT030 < Formula
+  desc "HTTP/1.1 server implementing mock responses for testing of edge cases"
+  homepage "https://github.com/simonmittag/mse6"
+  url "https://github.com/simonmittag/mse6/archive/v0.3.0.tar.gz"
+  sha256 "7fa0d14082a2499b1e3660a0bf5548c16d8ddd0de0e06c61155580ad9c2a4acb"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = HOMEBREW_CACHE / "go_cache"
+    (buildpath / "src/github.com/simonmittag/mse6").install buildpath.children
+
+    cd "src/github.com/simonmittag/mse6" do
+      system "go", "install", "github.com/simonmittag/mse6"
+      system "go", "install", "github.com/simonmittag/mse6/cmd/mse6"
+      bin.install HOMEBREW_CACHE / "go_cache/bin/mse6"
+    end
+  end
+
+  test do
+    assert_match "pass", shell_output("mse6 -t 2>&1")
+  end
+end
