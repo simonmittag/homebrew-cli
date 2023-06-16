ff372fc28db0084187a49f597377334b87e624b5
simonmittag <simonmittag@gmail.com>
2023-06-16 12:21:30 +1000

    Brew formula update for punycoder version v0.2.2

--- punycoder.rb
+++ punycoder.rb
@@ -5,11 +5,11 @@
 class Punycoder < Formula
   desc ""
   homepage "https://github.com/simonmittag/punycoder"
-  version "0.2.1"
+  version "0.2.2"
 
   on_macos do
-    url "https://github.com/simonmittag/punycoder/releases/download/v0.2.1/punycoder_0.2.1_darwin_all.tar.gz"
-    sha256 "5009f7d933243a7007f82e90b884156c73aa75dfc6c2688b78c8cda2c9eefdc0"
+    url "https://github.com/simonmittag/punycoder/releases/download/v0.2.2/punycoder_0.2.2_darwin_all.tar.gz"
+    sha256 "f709e9b73f378d068a129bc1ac32133f8be503bea9904251904a8ed48071fecf"
 
     def install
       bin.install "punycoder"
@@ -17,17 +17,17 @@ class Punycoder < Formula
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/simonmittag/punycoder/releases/download/v0.2.1/punycoder_0.2.1_linux_arm64.tar.gz"
-      sha256 "e5e9499bb7a2e9649d3b32804bec0654750ed3b7a335c4861bbe7d0300adf069"
+    if Hardware::CPU.intel?
+      url "https://github.com/simonmittag/punycoder/releases/download/v0.2.2/punycoder_0.2.2_linux_amd64.tar.gz"
+      sha256 "aaa7101f76a86706bd2dfa1d512b2c721bd56a2b1604fe4cf48ba277a76a7d1a"
 
       def install
         bin.install "pwt"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/simonmittag/punycoder/releases/download/v0.2.1/punycoder_0.2.1_linux_amd64.tar.gz"
-      sha256 "49002bfde1c16f195c18dcbd6b7cf12cfc203c660f824b9d1bfd25eb5281b460"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/simonmittag/punycoder/releases/download/v0.2.2/punycoder_0.2.2_linux_arm64.tar.gz"
+      sha256 "82edf6f4b5d098b7152637d4628773942ca4ba13433aa81dee87baf53f93809f"
 
       def install
         bin.install "pwt"
